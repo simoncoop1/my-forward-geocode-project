8b00ab6d4fcfbe681d09547f7dc1edcf87ec4855
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-07-15 21:09:33 +0100

    Delete my-coor-wrapper.py
    made a utility script for to coordinate conv, that contained various different available methods. Called 'my-coord-util.py'

--- my-coor-wrapper.py
+++
@@ -1,8 +0,0 @@
-import os    
-
-cmd = """./giqtrans --CGI='SourceSRID=27700&TargetSRID=4937&PreferredDatum=13&Geometry={"type":"Point","coordinates":[586500,245739,0]}'"""
-
-
-#call = os.system("cat syscall_list.txt | grep f89e7000 | awk '{print $2}'")
-call = os.system(cmd)
-print(call)
